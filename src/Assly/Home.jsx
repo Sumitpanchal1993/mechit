b0a551dfa74fa506098dab7f213dc2013408e077
Sumit Panchal <112932064+Sumitpanchal1993@users.noreply.github.com>
2024-02-05 23:49:02 +0530

    Home page updated

--- src/Assly/Home.jsx
+++ src/Assly/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
   return (
     <>
       <div className="homecontainer">
-        <div className="home_lhs">
+        {/* <div className="home_rhs">
           <div className="title">
             <h1>Hi ! Visitor This is:</h1>
             <p className="myHeading">Sumit Panchal</p>
@@ -32,10 +32,28 @@ function Home() {
               <Link to="/contact">Hire Me</Link>
             </div>
           </div>
-        </div>
-        <div className="home_rhs">
+        </div> */}
+        <div className="home_lhs">
           <div className="poster">
             <img src={Poster} alt="" />
+          </div>
+        </div>
+        <div className="home_rhs">
+          <div className="title">
+            <h1>Hi ! Visitor This is:</h1>
+            <p className="myHeading">Sumit Panchal</p>
+            <p className="myBio">Web Developer and a Mechanical Engineer</p>
+          </div>
+
+          <div className="exp_btn">
+            <button onClick={aboutMe}>Explore About Me</button>
+            <div className="explore_menu">
+              <Link to="/qualification">Qualification</Link>
+              <Link to="/experience">Experience</Link>
+              <Link to="/project">Projects</Link>
+              <Link to="/contact">Contact Me</Link>
+              <Link to="/contact">Hire Me</Link>
+            </div>
           </div>
         </div>
       </div>
