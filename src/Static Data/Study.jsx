bc52569df238a4eb08989486697b4d8a2c7a7a37
Sumitpanchal1993 <sumitpanchal08@gmail.com>
2024-07-11 20:26:49 +0530

    Education Card Changed

--- src/Static Data/Study.jsx
+++ src/Static Data/Study.jsx
@@ -1,9 +1,9 @@
 const edu_data = [
-    { id: 1, tilte: 'Master of Business Administration', school: 'Prestige institute of Management and Research', city: 'Dewas M.P.', splz: 'System Information and Marketting', year: 2023, percent: 72.62, link:'MBA.pdf' },
-    { id: 2, tilte: 'Bachelor of Engineering', school: 'Mahakal Institute of Technology and Science', city: 'Ujjain M.P.', splz: 'Mechanical Engineering', year: 2015, percent: 73.20, link:'BE.jpg'  },
-    { id: 3, tilte: 'Higher Secondary School Certificate Exam (12th)', school: 'School For Excellence', city: 'Dewas M.P.', splz: 'Mathematics', year: 2011, percent: 80.80, link:'12.jpg'  },
-    { id: 4, tilte: 'High School Certificate Exam (10th)', school: 'School For Excellence', city: 'Dewas M.P.', splz: 'All Subjects', year: 2009, percent: 81.83, link:'10.jpg'  },
-    { id: 5, tilte: 'Post Graduate Diploma in Computer Application', school: 'Makhanlal Chaturvedi University', city: 'Dewas M.P.', splz: 'Computer Application', year: 2017, percent: 65.4, link:'PG.jpg'  }
+    { id: 1, about: 'Master of Business Administration', comp_name: 'Prestige institute of Management and Research', location: 'Dewas M.P.', splz: 'System Information and Marketting', year: 2023, percent: 72.62, link:'MBA.pdf' },
+    { id: 2, about: 'Bachelor of Engineering', comp_name: 'Mahakal Institute of Technology and Science', location: 'Ujjain M.P.', splz: 'Mechanical Engineering', year: 2015, percent: 73.20, link:'BE.jpg'  },
+    { id: 3, about: 'Higher Secondary comp_name Certificate Exam (12th)', comp_name: 'comp_name For Excellence', location: 'Dewas M.P.', splz: 'Mathematics', year: 2011, percent: 80.80, link:'12.jpg'  },
+    { id: 4, about: 'High comp_name Certificate Exam (10th)', comp_name: 'comp_name For Excellence', location: 'Dewas M.P.', splz: 'All Subjects', year: 2009, percent: 81.83, link:'10.jpg'  },
+    { id: 5, about: 'Post Graduate Diploma in Computer Application', comp_name: 'Makhanlal Chaturvedi University', location: 'Dewas M.P.', splz: 'Computer Application', year: 2017, percent: 65.4, link:'PG.jpg'  }
   ]
 
 
