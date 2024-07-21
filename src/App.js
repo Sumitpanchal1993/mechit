8e77dbec9a5a010aee6d66b3029552f85a1df2a0
Sumitpanchal1993 <sumitpanchal08@gmail.com>
2024-07-21 23:05:22 +0530

    Bug of Screen Removed

--- src/App.js
+++ src/App.js
@@ -12,24 +12,13 @@ import { useState } from "react";
 
 
 function App() {
-  const [wid, setWid] = useState(0)
-  const [hei, setHei] = useState(0)
-
-  const detail = ()=>{
-    setWid(window.innerWidth)
-    setHei(window.innerHeight)
-  }
+  
 
   return (
     <>
       <Navbar />
       <Heros />
       <About/>
-      <div>
-        <button onClick={detail}> click Me</button>
-        <h1>{wid}</h1>
-        <h1>{hei}</h1>
-      </div>
       <Myskills />
       <Projects />
       <Education/>
